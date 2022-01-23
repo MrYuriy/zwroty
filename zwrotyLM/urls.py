2d0006b7c337ef263710908c8de1a86618b7a1c3
MrYuriy <90417593+MrYuriy@users.noreply.github.com>
2022-01-24 00:47:18 +0200

    Update urls.py

--- zwrotyLM/urls.py
+++ zwrotyLM/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('gen_protocol.urls')),
     #path('', include('account.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
