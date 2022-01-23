248ed8a574c1ffef2293517faffa77d875a6d1a0
MrYuriy <90417593+MrYuriy@users.noreply.github.com>
2022-01-23 22:51:17 +0200

    Update settings.py

--- zwrotyLM/settings.py
+++ zwrotyLM/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -137,6 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # db_from_env = dj_database_url.config()
 # DATABASES['default'].update(db_from_env)
