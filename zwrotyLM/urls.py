2382e77031ab18aa4cf26695ed3b29ae49119699
MrYuriy <90417593+MrYuriy@users.noreply.github.com>
2022-01-24 00:51:10 +0200

    Update urls.py

--- zwrotyLM/urls.py
+++ zwrotyLM/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
 #from gen_protocol import urls
 
 urlpatterns = [
